cce4d48968bbe7af59a7e18db2386b226214c3ba
sally duan <sduan@globys.com>
2022-11-27 00:45:32 -0800

    checking in a good working client

---
+++ api/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+using api.DTOs;
+using api.Entities;
+
+namespace api.Interfaces
+{
+    public interface IUserRepository
+    {
+         void Update(AppUser user);
+         Task<bool> SaveAllAsync();
+         Task<IEnumerable<AppUser>> GetUsersAsync();
+         Task<AppUser> GetUserByIdAsync(int id);
+         Task<AppUser> GetUser(string username);
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<MemberDto> GetMemberAsync(string username);        
+    }
+}
---
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
+import { ActivatedRoute} from '@angular/router';
+
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+ member:Member|undefined;
+  constructor(private memberService:MembersService, private route:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember()
+  {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.memberService.getMember(username).subscribe({
+      next:member=>this.member = member  
+    });
+    
+  }
+
+}
